3246a6cc59e6bfa8adc142e29082cea90e7e7952
bndr1ksa <bndr5ksa@gmail.com>
2025-08-01 01:17:41 +0300

    Chat app

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\MessageController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+// Route::post('/messages', [MessageController::class, 'store']);
+
+
+// Route::get('/test-broadcast-driver', function () {
+//     return config('broadcasting.default'); // يجب أن ترجع "reverb"
+// });
